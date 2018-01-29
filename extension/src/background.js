e57c6348aa5ee7faf9a7da19eedb87edb03be3ad
Cezary Piątek <cepi@ailleron.com>
2018-01-29 23:05:16 +0100

    Add command

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,19 +1,28 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.query({}, function(tabs){
-		chrome.tabs.sendMessage(tab.id, {
-			command: "show_tabswitcher",
-				allTabs: tabs		
-		  },
-		  function() {
-		  });
+(function () {
+	function showTabSwitcher(tab) {
+		chrome.tabs.query({}, function (tabs) {
+			chrome.tabs.sendMessage(tab.id, {
+				command: "show_tabswitcher",
+				allTabs: tabs
+			}, function () {});
+		});
+	}
+	chrome.browserAction.onClicked.addListener(function (tab) {
+		showTabSwitcher(tab);
 	});
-});
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if(request.command == "select_tab")
-		{
-			chrome.tabs.update(request.tab.id, {active: true});
+	chrome.commands.onCommand.addListener(function (command) {
+		if (command == "show-tabswitcher") {
+			chrome.tabs.getSelected(null, function(tab){
+				showTabSwitcher(tab);
+			});
+		}
+	});
+
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.command == "select_tab") {
+			chrome.tabs.update(request.tab.id, { active: true });
 			sendResponse();
 		}
 	});
+})();
